9d61abef0793f85f02ac2fcc4c5bfc44c5d6b801
didrika@protonmail.com <didrika@protonmail.com>
2020-09-08 07:05:54 -0700

    Console UI skeleton created

--- Excercises/Program.cs
+++ Excercises/Program.cs
@@ -6,7 +6,59 @@ namespace Excercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var keepAlive = true;
+            while(keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (-1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExcerciseOne();
+                            break;
+                        case 2:
+                            RunExcerciseTwo();
+                            break;
+                        case 3:
+                            RunExcerciseThree();
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number.");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid assignment number!");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static void RunExcerciseOne()
+        {
+            Console.WriteLine("You successfully ran excercise one!");
+        }
+        private static void RunExcerciseTwo()
+        {
+            Console.WriteLine("You successfully ran excercise two!");
+        }
+        private static void RunExcerciseThree()
+        {
+            Console.WriteLine("You successfully ran excercise three!");
         }
     }
 }
