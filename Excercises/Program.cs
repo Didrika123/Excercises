eb053aa8283e452d8be2281933a63176ee1b9a98
didrika@protonmail.com <didrika@protonmail.com>
2020-09-08 07:49:13 -0700

    Finished methods for excercise 3 & 4

--- Excercises/Program.cs
+++ Excercises/Program.cs
@@ -26,6 +26,15 @@ namespace Excercises
                         case 3:
                             RunExcerciseThree();
                             break;
+                        case 4:
+                            RunExcerciseFour();
+                            break;
+                        case 5:
+                            RunExcerciseFive();
+                            break;
+                        case 6:
+                            RunExcerciseSix();
+                            break;
                         case -1:
                             keepAlive = false;
                             break;
@@ -62,14 +71,44 @@ namespace Excercises
             DateTime dateOfToday = DateTime.Now, 
                      dateOfYesterday = dateOfToday.AddDays(-1), 
                      dateOfTomorrow = dateOfToday.AddDays(1);
-            Console.WriteLine($"Todays date is {dateOfToday.ToShortDateString()}\n");
+            Console.WriteLine($"Today's date is {dateOfToday.ToShortDateString()}\n");
             Console.WriteLine($"Tomorrow's date is {dateOfTomorrow.ToShortDateString()}\n");
             Console.WriteLine($"Yesterday's date is {dateOfYesterday.ToShortDateString()}\n");
             // subtracting two dates, you get back a TimeSpan-object with interval data
         }
         private static void RunExcerciseThree()
         {
-            Console.WriteLine("You successfully ran excercise three!");
+            Console.WriteLine("You successfully ran excercise three!\n");
+            Console.ResetColor();
+
+            string firstName = "<firstname>", lastName = "<lastname>";
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine($"\nHello {firstName} {lastName}!\n");
+        }
+        private static void RunExcerciseFour()
+        {
+            Console.WriteLine("You successfully ran excercise four!\n");
+            Console.ResetColor();
+
+            string str = "The quick fox Jumped Over the DOG";
+            //Transform into “The brown fox jumped over the lazy dog”
+            string newStr = "T" + str.Substring(1).ToLower().Replace("quick", "brown").Replace("dog", "lazy dog");
+            Console.WriteLine(newStr + "\n");
+        }
+        private static void RunExcerciseFive()
+        {
+            Console.WriteLine("You successfully ran excercise five!\n");
+            Console.ResetColor();
+
+        }
+        private static void RunExcerciseSix()
+        {
+            Console.WriteLine("You successfully ran excercise six!\n");
+            Console.ResetColor();
+
         }
     }
 }
